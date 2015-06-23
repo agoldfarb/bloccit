5a2c6e97cd311ffe243b043fb0a96f0f574a3f7c
Allison Goldfarb <allisonrgoldfarb@gmail.com>
2015-06-23 13:48:26 -0500

    checkpoint 27 completed, bloccit static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
+  get 'welcome/about'
+
+  root to: 'welcome#index'
+
+  # get "welcome/index"
+  # get "welcom/about"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
