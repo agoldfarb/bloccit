f33843d1820bad1c38e0f4c2c86700a4bc6ec826
Allison Goldfarb <allisonrgoldfarb@gmail.com>
2015-07-07 17:17:55 -0500

    checkpoint 40 completed, bloccit topics

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150707200844) do
+ActiveRecord::Schema.define(version: 20150707212418) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -36,8 +36,10 @@ ActiveRecord::Schema.define(version: 20150707200844) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
+    t.integer  "topic_id"
   end
 
+  add_index "posts", ["topic_id"], name: "index_posts_on_topic_id"
   add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
   create_table "questions", force: :cascade do |t|
@@ -46,6 +48,14 @@ ActiveRecord::Schema.define(version: 20150707200844) do
     t.boolean  "resolved"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "topics", force: :cascade do |t|
+    t.string   "name"
+    t.boolean  "public"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "users", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,22 @@
 require 'faker'
-
-# Create Users
   5.times do
     user = User.new(
      name:     Faker::Name.name,
      email:    Faker::Internet.email,
      password: Faker::Lorem.characters(10)
-    )
-   user.skip_confirmation!
-   user.save!
+  )
+  user.skip_confirmation!
+  user.save!
+end
+users = User.all
+
+ 15.times do
+  Topic.create!(
+    name:         Faker::Lorem.sentence,
+    description:  Faker::Lorem.paragraph
+  )
   end
-  users = User.all
+  topics = Topic.all
  
  # Note: by calling `User.new` instead of `create`,
  # we create an instance of User which isn't immediately saved to the database.
@@ -24,9 +30,10 @@ require 'faker'
  50.times do
    Post.create!(
      user:   users.sample,
+     topic:  topics.sample,
      title:  Faker::Lorem.sentence,
      body:   Faker::Lorem.paragraph
-   )
+    )
  end
  posts = Post.all
  
@@ -40,12 +47,33 @@ require 'faker'
  end
 
 
- user = User.first
- user.skip_reconfirmation!
- user.update_attributes!(
-   email: 'allisonrgoldfarb@gmail.com',
+ admin = User.new(
+   name:     'Admin User',
+   email:    'admin@example.com',
+   password: 'helloworld',
+   role:     'admin'
+ )
+ admin.skip_confirmation!
+ admin.save!
+ 
+ # Create a moderator
+ moderator = User.new(
+   name:     'Moderator User',
+   email:    'moderator@example.com',
+   password: 'helloworld',
+   role:     'moderator'
+ )
+ moderator.skip_confirmation!
+ moderator.save!
+ 
+ # Create a member
+ member = User.new(
+   name:     'Member User',
+   email:    'member@example.com',
    password: 'helloworld'
  )
+ member.skip_confirmation!
+ member.save!
  
  puts "Seed finished"
  puts "#{User.count} users created"
