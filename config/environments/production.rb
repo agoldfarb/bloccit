c49206a6619a1f6e44352d18a2098f658f75c647
Allison Goldfarb <allisonrgoldfarb@gmail.com>
2015-07-09 14:29:17 -0500

    configured app for production emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,6 +76,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-end
 
 config.action_mailer.default_url_options = { host: 'allisongoldfarb-bloccit.herokuapp.com' }
+end
