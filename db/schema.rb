648297b7f0eba2fdd0fae81bf27c0f3832850547
Allison Goldfarb <allisonrgoldfarb@gmail.com>
2015-07-12 13:37:51 -0500

    db rollback assignment-40

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150709200920) do
+ActiveRecord::Schema.define(version: 20150709184633) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -37,10 +37,8 @@ ActiveRecord::Schema.define(version: 20150709200920) do
     t.datetime "updated_at", null: false
     t.integer  "user_id"
     t.integer  "topic_id"
-    t.integer  "summary_id"
   end
 
-  add_index "posts", ["summary_id"], name: "index_posts_on_summary_id"
   add_index "posts", ["topic_id"], name: "index_posts_on_topic_id"
   add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
