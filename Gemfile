0533d8e4b4c6464978665161e01ea622af3f7303
Allison Goldfarb <allisonrgoldfarb@gmail.com>
2015-07-12 18:33:33 -0500

    checkpoint 44 completed, bloccit pagination

--- Gemfile
+++ Gemfile
@@ -35,6 +35,7 @@ gem 'redcarpet'
 gem 'carrierwave'
 gem 'mini_magick'
 gem 'fog'
+gem 'will_paginate', '~> 3.0.5'
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
 gem 'turbolinks'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,6 +1,6 @@
 class TopicsController < ApplicationController
   def index
-    @topics = Topic.all
+    @topics = Topic.paginate(page: params[:page], per_page: 10)
     authorize @topics
   end
 
@@ -11,8 +11,7 @@ class TopicsController < ApplicationController
 
   def show
     @topic = Topic.find(params[:id])
-    @posts = @topic.posts
-    authorize @topic
+    @posts = @topic.posts.paginate(page: params[:page], per_page: 10)
   end
 
   def edit
