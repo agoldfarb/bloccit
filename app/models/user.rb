b6353e1d8802563c0a9bfb4ba9bf9d50b87cd06f
Allison Goldfarb <allisonrgoldfarb@gmail.com>
2015-07-20 15:54:54 -0500

    checkpoint 46 completed, bloccit comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
     role == 'moderator'
   end
 
-  
+  has_many :comments
   has_many :posts
   mount_uploader :avatar, AvatarUploader
   # Include default devise modules. Others available are:
--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150713200745) do
+ActiveRecord::Schema.define(version: 20150716011708) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
     t.integer  "post_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer "user_id"
   end
 
   add_index "comments", ["post_id"], name: "index_comments_on_post_id"
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id"
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
@@ -69,6 +71,7 @@ ActiveRecord::Schema.define(version: 20150713200745) do
     t.datetime "updated_at",                          null: false
     t.string   "role"
     t.string   "avatar"
+    t.integer  "user_id"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
