67913f9106f21947fbda89613525a77374e31915
Allison Goldfarb <allisonrgoldfarb@gmail.com>
2015-07-23 17:58:35 -0500

    assignment 51 completed, bloccit favoriting

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,14 +1,22 @@
 require 'rails_helper'
  
- describe User do
+  describe User do
  
   include TestFactories
  
   describe "#favorited(post)" do
-    xit "returns `nil` if the user has not favorited the post" do
+
+    before do
+      @user = authenticated_user
+      @post = authenticated_post
+    
+    it "returns `nil` if the user has not favorited the post" do
+      expect(@user.favorited(@post)).to be_nil
     end
  
-    xit "returns the appropriate favorite if it exists" do
+    it "returns the appropriate favorite if it exists" do
+      favorite = @user.favorites.create(post: @post)
+      expect(@user.favorited(@post)).to eq(favorite)
     end
   end
 end
