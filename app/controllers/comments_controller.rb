7e65c99ef8c116651d170e21e107eb69294d5615
Allison Goldfarb <allisonrgoldfarb@gmail.com>
2015-07-27 21:37:53 -0500

    checkpoint 57 completed, destroy w/ ajax

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -23,17 +23,24 @@ class CommentsController < ApplicationController
     @comment = @post.comments.find(params[:id])
  
     authorize @comment
+
     if @comment.destroy
       flash[:notice] = "Comment was removed."
-      redirect_to [@post.topic, @post]
     else
       flash[:error] = "Comment couldn't be deleted. Try again."
-      redirect_to [@post.topic, @post]
+    end
+
+    respond_to do | format |
+      format.html
+      format.js
     end
   end
 
+
   private
+
   def comment_params
     params.require(:comment).permit(:body)
   end
+
 end
