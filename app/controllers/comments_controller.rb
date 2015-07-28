bc9560e85ceb0b6532fb3f4b6785ff2d52e5f384
Allison Goldfarb <allisonrgoldfarb@gmail.com>
2015-07-27 21:58:33 -0500

    checkpoint 57 completed, creating w/ ajax

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,16 +1,24 @@
 class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
-    @comment = current_user.comments.build(comment_params)
-    @comment.post_id = @post.id 
+    @comments = @post.comments
+
+    @comment = Comment.new( comment_params )
+    @comment.user = current_user
+    @comment.post = @post
+    @new_comment = Comment.new
+
     authorize @comment
 
     if @comment.save
       flash[:notice] = "Comment was saved."
-      redirect_to [@post.topic, @post]
     else
       flash[:error] = "There was an error saving the comment. Please try again."
-      render :new
+    end
+
+    respond_to do | format |
+      format.html
+      format.js
     end
   end
 
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,7 @@
 class Comment < ActiveRecord::Base
+
+  default_scope { order('updated_at DESC') }
+  
   belongs_to :post
   belongs_to :user
 
